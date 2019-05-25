fbab42558f1b8a57527e136605e76b4041d75c29
Evelyn Sjafii <evelynsj98@gmail.com>
2019-05-25 15:47:06 -0700

    Outline

---
+++ raw_descriptor_matches.m
@@ -0,0 +1,7 @@
+% Use the two images and associated features in provided file
+% twoFrameData.mat
+%% Allow a user to select a region of interest in one frame using selectRegion.m
+
+%% Match descriptors in that region to descriptors in second image based on Euclidean distance in SIFT space
+
+%% Display selected region of interest in the image (polygon) and the matched features in the second image
