53cff6cf54c180a29d807d9c77bd737169665068
Evelyn Sjafii <evelynsj98@gmail.com>
2019-06-03 14:35:51 -0700

    Create histogram for three query images

--- full_frame_queries.m
+++ full_frame_queries.m
@@ -20,10 +20,8 @@
         % Take minimum across the rows of result (of dist2) and note down
         % the indices of all the minimums. Can do this in one line with the
         % min function ==> membership array
-        % Determine which word out of the 1500 words the minimum distance
-        % corresponds to
-        % the value in the membership matrix is just the index in the
-        % descriptor (so max is 1500)
+        % Determine which word out of the 1500 words the minimum distance corresponds to
+        % the value in the membership matrix is just the index of the word descriptor (so max is 1500)
     % range = 1:1500
     
 %% Choose 3 different/random frames to serve as queries
@@ -45,15 +43,21 @@ for i=1:3
 end
 
 %% Create bag of words for each query image
-for i=1:1
+
+queryBow = [];
+for i=1:3
     % get the descriptors
     fname = [siftdir '/' fnames(randFrames(i)).name];
     load(fname, 'descriptors');
     % find the distances between each descriptor and the kmeans clusters (use dist2)
     dist = dist2(descriptors, means); % each row values are the distance from each descriptor to all 1500 words hence nx1500
     % Get the membership vector
-    [min, minIdx] = min(dist, [], 2);
+    [~, minIdx] = min(dist, [], 2);
+    % create a histogram
+    bincounts = histc(minIdx, 1:1500);
+    queryBow = [queryBow; bincounts'];
 end
+% BoW for the three query images are stored in queryBow (each row corresponds to each image)
 
 %% thoughts
 % we need to use dist to find membership for the query images because the
