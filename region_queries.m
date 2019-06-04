bfdd4f021977c00b40d1bd4a24dc16b3d331eeea
Evelyn Sjafii <evelynsj98@gmail.com>
2019-06-03 21:28:46 -0700

    Added outline

---
+++ region_queries.m
@@ -0,0 +1,6 @@
+%% rough outline
+% select query region inside frame
+    % this means that we use only a portion of the descriptors inside a frame, not all
+    % create histograms for these descriptors
+% compute similarity scores between the query image and remaining images
+% do this for four frames
